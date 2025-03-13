828edee5300ab5153851927054bebfcab7577455
Rafael <raas@cin.ufpe.br>
2025-03-13 18:29:45 -0300

    adicionando infiniteScroll

--- frontend/src/components/catalog.tsx
+++ frontend/src/components/catalog.tsx
@@ -1,12 +1,20 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
 import Product from "./productAdmin";
 import { motion } from "framer-motion";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+interface ProductType {
+    name: string;
+    price: number;
+    url_image: string;
+}
 
 const Catalog: React.FC = () => {
+    const [products, setProducts] = useState<ProductType[]>([]);
+    const [hasMore, setHasMore] = useState(true);
+    const [page, setPage] = useState(1);
 
-    
-
-    const products = [
+    const allProducts = [
         {
             name: "Produto 1",
             price: 100,
@@ -54,19 +62,44 @@ const Catalog: React.FC = () => {
         }
     ];
 
+    const fetchMoreData = () => {
+        if (products.length >= allProducts.length) {
+            setHasMore(false);
+            return;
+        }
+        setTimeout(() => {
+            setProducts(products.concat(allProducts.slice(products.length, products.length + 3)));
+            setPage(page + 1);
+        }, 1500);
+    };
+
+    useEffect(() => {
+        setProducts(allProducts.slice(0, 6));
+    }, []);
+
     return (
         <div>
-            <motion.div className="flex flex-wrap items-center justify-center"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1 }}
+            <motion.div className="flex items-center justify-center text-[#111111]"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1 }}
             >
-               {products.map((products, index) => (
-                <Product key={index} product={products} />
-                ))} 
+                <InfiniteScroll
+                    dataLength={products.length}
+                    next={fetchMoreData}
+                    hasMore={hasMore}
+                    loader={<h4 className="text-center">Carregando...</h4>}
+                    endMessage={<h4 className="text-center">Você já viu tudo!!</h4>}
+                >
+                    <div className="flex flex-wrap justify-center gap-4">
+                        {products.map((product, index) => (
+                            <Product key={index} product={product} />
+                        ))}
+                    </div>
+                </InfiniteScroll>
             </motion.div>
-        </div>     
-    )
+        </div>
+    );
 }
 
 export default Catalog;
