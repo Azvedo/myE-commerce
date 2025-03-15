3547fc9f1e792c41b41aa346d1f3ad03287622b1
Rafael <raas@cin.ufpe.br>
2025-03-14 22:51:33 -0300

    adicionando tela de login e autenticação para acessar adminPage

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,13 +3,21 @@ import { Route, Routes } from "react-router-dom";
 import Admin from "./screens/adminPage";
 import { ProductUpdateProvider } from "./context/useProductUpdate";
 import ClientPage from "./screens/clientPage";
+import Login from "./screens/login";
+import ProtectedRoute from "./protectedRoute";
+
 
 const AppRoutes : React.FC = () => {
     return (
         <ProductUpdateProvider>
             <Routes>
                 <Route path="/" element={<ClientPage/>} />
-                <Route path="/admin" element={<Admin />} />
+                <Route path="/login" element={<Login/>} />
+                <Route path="/admin" element={
+                    <ProtectedRoute>
+                        <Admin/>
+                    </ProtectedRoute>
+                    } />
             </Routes>
         </ProductUpdateProvider>
     )
